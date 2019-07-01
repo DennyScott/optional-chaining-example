563e9cec3b52cdca3c6208d255c2343849075ea3
Denny Scott <dennyscott301@gmail.com>
2019-07-01 15:45:46 -0700

    Add babel and webpack routes

---
+++ babel/index.js
@@ -0,0 +1,20 @@
+const personOne = {
+  firstName: "Denny",
+  lastName: "Scott",
+  contact: {
+    phoneNumber: 555-555-5555,
+    twitter: "@gitinbit"
+  }
+}
+
+function getUserForPhoneDirectory(person) {
+  const firstName = person?.firstName;
+  const lastName = person?.lastName;
+  const phoneNumber = person?.contact?.phoneNumber;
+
+  if(firstName && lastName && phoneNumber) {
+    return `${firstName} ${lastName} - ${phoneNumber}`
+  }
+}
+
+getUserForDirectory(personOne);  //expect: Denny Scott - 555-555-5555
